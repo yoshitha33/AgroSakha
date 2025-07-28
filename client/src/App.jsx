0a8f507edcb378640d37d49e2ea69cae2db45f03
yoshitha33 <yoshithanunna77@gmail.com>
2025-07-28 18:09:47 +0530

    Installation completed

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return <div className='text-center'>Hello</div>
+}
+
+export default App
